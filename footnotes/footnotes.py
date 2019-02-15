9a9606c162e537e7e959a3582f72362fe55a495d
Patrick Hulin <patrick.hulin@gmail.com>
2019-02-14 22:49:03 -0500

    Add docstrings.

--- footnotes/footnotes.py
+++ footnotes/footnotes.py
@@ -11,16 +11,9 @@ NS = {
 def ns(prefix, tag_name):
     return '{{{}}}{}'.format(NS[prefix], tag_name)
 
-class Paragraph(object):
-    def __init__(self, element):
-        assert element.tag == ns('w', 'p')
-        self.element = element
-        self.runs = [Run(e) for e in self.element.findall('w:r', NS)]
-
-    def text(self):
-        return ''.join([r.text() for r in self.runs])
-
 class Run(object):
+    """Represents a <w:r> element, a run of identically-formatted text."""
+
     def __init__(self, element):
         assert element.tag == ns('w', 'r')
         self.element = element
@@ -30,24 +23,50 @@ class Run(object):
         self.props = props[0] if props else None
 
     def italics(self):
+        """Is this text in italics?"""
+
         return bool(self.props.findall('w:i', NS)) or \
             bool(self.props.findall('w:u', NS))
 
     def smallcaps(self):
+        """Is this text in small caps?"""
+
         return bool(self.props.findall('w:smallCaps', NS))
 
     def text(self):
+        """Unformatted text for run."""
+
         text_elem = self.element.findall('w:t', NS)
         return text_elem[0].text if text_elem else ''
+
+class Paragraph(object):
+    """Represents a <w:r> element, a paragraph."""
+
+    def __init__(self, element):
+        assert element.tag == ns('w', 'p')
+        self.element = element
+        self.runs = [Run(e) for e in self.element.findall('w:r', NS)]
+
+    def text(self):
+        """Unformatted text for paragraph."""
+
+        return ''.join([r.text() for r in self.runs])
 
 class Footnote(object):
     def __init__(self, element):
         assert element.tag == ns('w', 'footnote')
         self.element = element
-        self.paras = [Paragraph(e) for e in self.element.findall('w:p', NS)]
+        self.paragraphs = [Paragraph(e) for e in self.element.findall('w:p', NS)]
 
     def id(self):
+        """Footnote id. Should be the same as the number you see in the document."""
+
         return int(self.element.get(ns('w', 'id')))
+
+    def text(self):
+        """Unformatted text for footnote."""
+
+        return ''.join([p.text() for p in self.paragraphs])
 
 class FootnoteList(object):
     def __init__(self, tree):
@@ -59,6 +78,8 @@ class FootnoteList(object):
 
     @staticmethod
     def from_file(f):
+        """Return a FootnoteList from filename or file object."""
+
         return FootnoteList(ET.parse(f))
 
     @staticmethod
