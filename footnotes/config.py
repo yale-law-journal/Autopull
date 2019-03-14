5735b69b42e8e220e1a69a8bfa348b77c42f5cbd
Patrick Hulin <patrick.hulin@gmail.com>
2019-03-14 14:14:18 -0400

    Add option for development config.

--- footnotes/config.py
+++ footnotes/config.py
@@ -1,5 +1,9 @@
 import json
 from os.path import dirname, join
 
-with open(join(dirname(__file__), 'config.json')) as config_f:
-    CONFIG = json.load(config_f)
+try:
+    with open(join(dirname(__file__), 'config_development.json')) as config_f:
+        CONFIG = json.load(config_f)
+except FileNotFoundError:
+    with open(join(dirname(__file__), 'config.json')) as config_f:
+        CONFIG = json.load(config_f)
